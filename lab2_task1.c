e1ebe7c7361522e4adee09fc5822ed0d13b5794d
Эмир <torimir1977@yandex.ru>
2024-09-05 20:51:43 +0300

    .

--- lab2_task1.c
+++ lab2_task1.c
@@ -2,52 +2,47 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main(void)
+int main()
 {
-	double start, end; // объявляем переменные для определения времени выполнения
-	
-	int i=0, j=0, r;
-	int a[200][200], b[200][200], c[200][200], elem_c;
-
-	srand(time(NULL)); // инициализируем параметры генератора случайных чисел
+	double start = 0, stop = 0;
+	int i = 0, j = 0, r = 0;
+	int a[1000][1000], b[1000][1000], c[1000][1000], elem_c;
+	srand(time(NULL));
 	start = clock();
-	while(i<200)
+	while (i < 1000)
 	{
-		while(j<200)
+		while (j < 1000)
 		{
-			a[i][j]=rand()% 100 + 1; // заполняем массив случайными числами // O(1)
+			a[i][j] = rand() % 100 + 1;
 			j++;
-		}	// O(n)
+		}
 		i++;
-	} 		// O(n^2)
-	srand(time(NULL)); // инициализируем параметры генератора случайных чисел
-	i=0; j=0;
-	while(i<200)
+	}
+	srand(time(NULL));
+	i = 0, j = 0;
+	while (i < 1000)
 	{
-		while(j<200) 
-		
-			b[i][j]=rand()% 100 + 1; // заполняем массив случайными числами
-			j++;
-		}	// O(n)
-		i++;
-	}		// O(n^2)
-
-	for(i=0;i<200;i++)
-	{
-		for(j=0;j<200;j++)
+		while (j < 1000)
 		{
-			elem_c=0;
-			for(r=0;r<200;r++)
+			b[i][j] = rand() % 100 + 1;
+			j++;
+		}
+		i++;
+	}
+	for (i = 0; i < 1000; i++)
+	{
+		for (j = 0; j < 1000; j++)
+		{
+			for ( r = 0; r < 1000; r++)
 			{
-				elem_c=elem_c+a[i][r]*b[r][j];
-				c[i][j]=elem_c;
-			} // O(n)
-		}	// O(n*n)
-	} 		// O(n^3)
-	// O(2 * O(n^2), O(n^3))
-	end = clock();
-	printf("Time of matrix multiplication: %lf sec.", (end - start) / 1000);
-   
-	return 0;
+				elem_c = elem_c + a[i][r] * b[i][r];
+				c[i][j] = elem_c;
+			}
+			
+		}
+		
+	}
+	stop = clock();
+	printf("Time of matrix multiply: %lf sec.\n", (stop - start) / 1000);
+	return 0;	
 }
-
