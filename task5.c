74aed0b8a76df36769451a48c32673bcdfddd974
Linus Torvalds <torvalds@linux-foundation.org>
2024-09-06 16:55:12 +0300

    final ver

--- task5.c
+++ task5.c
@@ -15,46 +15,45 @@ struct student
 int main()
 {
    struct student s1;
+   int n;
    for (int32_t i = 0; i < 3; i++)
    {
-      puts("Enter student's name, surname and major: ");
-      scanf("%s %s %d", stud[i].name, stud[i].surname, stud[i].major);
+      puts("Enter student's data (name, surname, major): ");
+      scanf("%s %s %d", stud[i].name, stud[i].surname, &stud[i].major);
       rewind(stdin); // Очистка буфера scanf
-      
    }
    for (int i = 0; i < 3; i++)
    {
       printf("%s\t%s\t%d\n", stud[i].name, stud[i].surname, stud[i].major);
    }
-   for (int n; n != 0; )
+   puts("1 - Begin search.");
+   puts("0 - Exit.\n> ");
+   scanf("%d", &n);
+   for (; n != 0; )
    {
-      puts("1 - Begin search.\n");
+      int8_t name[20] = "";
+      int8_t surname[20] = "";
+      int16_t major;
+      puts("Enter student's major: ");
+      scanf("%d", &major);
+      
+      puts("Enter student's name: ");
+      scanf("%20s", name);
+      rewind(stdin);
+      
+      puts("Enter student's surname: ");
+      scanf("%20s", surname);
+      rewind(stdin);
+
+      for (int i = 0; i < 3; i++)
+      {
+         if (strcmp(name, stud[i].name) == 0 and strcmp(surname, stud[i].surname) == 0 and major == stud[i].major)
+            printf("%s\t%s\t%d\n", stud[i].name, stud[i].surname, stud[i].major);
+      }
+      puts("1 - Begin search.");
       puts("0 - Exit.\n> ");
       scanf("%d", &n);
-      switch (n)
-      {
-      case 1:
-         int8_t name[20] = "";
-         int8_t surname[20] = "";
-         int16_t major;
-         puts("Enter student's name, surname and major: ");
-         scanf("%s %s %d",name, surname, &major);
-         for (int i = 0; i < 3; i++)
-         {
-            if (strcmp(name, stud[i].name) == 0 and strcmp(surname, stud[i].surname) == 0 and major == stud[i].major)
-            {
-               printf("%s\t%s\t%d\n", stud[i].name, stud[i].surname, stud[i].major);
-            }
-            
-         }
-          
-         break;
-      }
    }
-   puts("Exiting.\n> ");
-   
-   
-   
-   
+   puts("Exiting.\n");
    return 0;
 }
