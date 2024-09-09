b0ba5d0b1f9c4941e83601a3db1a6ac5ffdbe6c0
Эмир Ториев <torimir1977@yandex.ru>
2024-09-09 15:27:03 +0300

    final_ver

--- lab2_task1.c
+++ lab2_task1.c
@@ -1,48 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define MATRIX_SIZE 1000
+#define X 10000
+#define Y 10000
 int main()
 {
-	clock_t start = 0, stop = 0;
+	double start = 0, stop = 0;
 	int i = 0, j = 0, r = 0;
-	int a[MATRIX_SIZE][MATRIX_SIZE], b[MATRIX_SIZE][MATRIX_SIZE], c[MATRIX_SIZE][MATRIX_SIZE], elem_c;
+	int** a = (int**)calloc(X * Y, sizeof(int*)); // O(1)
+	int** b = (int**)calloc(X * Y, sizeof(int*)); // O(1)
+	int** c = (int**)calloc(X * Y, sizeof(int*)); // O(1)
+	int elem_c = 0;
 	srand(time(NULL));
-	start = clock();
-	while (i < MATRIX_SIZE)
+	for (i = 0; i < Y; i++) // O(N)
 	{
-		while (j < MATRIX_SIZE)
-		{
-			a[i][j] = rand() % 100 + 1;
-			j++;
-		}
-		i++;
+		a[i] = (int*)calloc(Y, sizeof(int)); // O(1)
+		b[i] = (int*)calloc(Y, sizeof(int)); // O(1)
+		c[i] = (int*)calloc(Y, sizeof(int)); // O(1)
 	}
-	srand(time(NULL));
 	i = 0, j = 0;
-	while (i < MATRIX_SIZE)
+	puts("Multiply...");
+	start = clock();
+	for (i = 0; i < X; i++)	// O(N^3)
 	{
-		while (j < MATRIX_SIZE)
+		for (j = 0; j < Y; j++)
 		{
-			b[i][j] = rand() % 100 + 1;
-			j++;
-		}
-		i++;
-	}
-	for (i = 0; i < MATRIX_SIZE; i++)
-	{
-		for (j = 0; j < MATRIX_SIZE; j++)
-		{
-			for ( r = 0; r < MATRIX_SIZE; r++)
+			a[i][j] = rand() % 100;
+			b[i][j] = rand() % 100;
+			for ( r = 0; r < Y; r++)
 			{
 				elem_c = elem_c + a[i][r] * b[i][r];
 				c[i][j] = elem_c;
 			}
-			
 		}
-		
 	}
 	stop = clock();
-	printf("Time of matrix multiply: %lf sec.\n", (stop - start) / CLOCKS_PER_SEC);
+	printf("Time of matrix %dx%d multiply : %lf sec.\n", X, Y, (stop - start) / CLOCKS_PER_SEC);
+	free(a);	// O(1)
+	free(b);	// O(1)
+	free(c);	// O(1)
+	getchar();
 	return 0;	
 }
