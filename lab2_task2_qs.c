66e7bcb678504404e9c2decc03ada9a04d57f443
Эмир <torimir1977@yandex.ru>
2024-09-10 15:18:39 +0300

    auto_measure

--- lab2_task2_qs.c
+++ lab2_task2_qs.c
@@ -27,7 +27,24 @@ void qs(int *items, int left, int right) //вызов функции: qs(items, 
   if(left < j) qs(items, left, j);
   if(i < right) qs(items, i, right);
 }
+void shell(int *items, int count)
+{
 
+  int i, j, gap, k;
+  int x, a[5];
+
+  a[0]=9; a[1]=5; a[2]=3; a[3]=2; a[4]=1;
+
+  for(k=0; k < 5; k++) {
+    gap = a[k];
+    for(i=gap; i < count; ++i) {
+      x = items[i];
+      for(j=i-gap; (x < items[j]) && (j >= 0); j=j-gap)
+        items[j+gap] = items[j];
+      items[j+gap] = x;
+    }
+  }
+}
 int funccmp (const void *v1, const void* v2)
 {
    return (*(int*)v1 - *(int*)v2);
@@ -37,23 +54,149 @@ int main()
    int n;
    double start, stop;
    srand(time(NULL));
-   puts("Enter array size: ");
-   scanf("%d", &n);
-   int* a = (int*)calloc(n, sizeof(int));
-   for (int i = 0; i < (n / 2); i++)
+   n = 1;
+   for (int i = 0; i < 4; i++)
    {
-      a[i] = i;
+      n *= 100;
+      int* a = (int*)calloc(n, sizeof(int));
+      FILE* t_measure = fopen("time.txt", "w+");
+      switch (i)
+      {
+      case 0:
+         for (int i = 0; i < n; i++)
+         {
+            a[i] = rand() % 50;
+         }
+         start = clock();
+         shell(a, n);
+         stop = clock();
+         fprintf(t_measure, "%d) Time of shell (random): %lf\n",i , (stop - start )/ CLOCKS_PER_SEC);
+         
+         for (int i = 0; i < n; i++)
+         {
+            a[i] = rand() % 50;
+         }
+         start = 0, stop = 0;
+         
+         start = clock();
+         qs(a, 0, n - 1);
+         stop = clock();
+         fprintf(t_measure, "%d) Time of qs (random): %lf\n", i, (stop - start )/ CLOCKS_PER_SEC);
+         
+         for (int i = 0; i < n; i++)
+         {
+            a[i] = rand() % 50;
+         }
+         start = 0, stop = 0;
+         
+         start = clock();
+         qsort(a, n, sizeof(int), funccmp);
+         stop = clock();
+         fprintf(t_measure, "%d) Time of qsort() (random): %lf\n", i, (stop - start )/ CLOCKS_PER_SEC);
+         
+         break;
+      case 1:
+         
+         for (int i = 0; i < n; i++)
+         {
+            a[i] = i;
+         }
+         start = clock();
+         shell(a, n);
+         stop = clock();
+         fprintf(t_measure, "%d) Time of shell (inc): %lf\n",i , (stop - start )/ CLOCKS_PER_SEC);
+         
+         for (int i = 0; i < n; i++)
+         {
+            a[i] = i;
+         }
+         start = 0, stop = 0;
+         
+         start = clock();
+         qs(a, 0, n - 1);
+         stop = clock();
+         fprintf(t_measure, "%d) Time of qs (inc): %lf\n", i, (stop - start )/ CLOCKS_PER_SEC);
+         
+         for (int i = n; i > 0; i--)
+         {
+            a[i] = i;
+         }
+         start = 0, stop = 0;
+         
+         start = clock();
+         qsort(a, n, sizeof(int), funccmp);
+         stop = clock();
+         fprintf(t_measure, "%d) Time of qsort() (inc): %lf\n", i, (stop - start )/ CLOCKS_PER_SEC);
+         break;   
+      case 2:
+         
+         for (int i = n; i > 0; i--)
+         {
+            a[i] = i;
+         }
+         start = clock();
+         shell(a, n);
+         stop = clock();
+         fprintf(t_measure, "%d) Time of shell (decr): %lf\n",i , (stop - start )/ CLOCKS_PER_SEC);
+         
+         for (int i = n; i > 0; i--)
+         {
+            a[i] = i;
+         }
+         start = 0, stop = 0;
+         start = clock();
+         qs(a, 0, n - 1);
+         stop = clock();
+         fprintf(t_measure, "%d) Time of qs (decr): %lf\n", i, (stop - start )/ CLOCKS_PER_SEC);
+         
+         for (int i = n; i > 0; i--)
+         {
+            a[i] = i;
+         }
+         start = 0, stop = 0;
+         
+         start = clock();
+         qsort(a, n, sizeof(int), funccmp);
+         stop = clock();
+         fprintf(t_measure, "%d) Time of qsort() (decr): %lf\n", i, (stop - start )/ CLOCKS_PER_SEC);
+         break;
+      case 3:
+         
+         for (int i = 0; i < (n / 2); i++)
+         {
+            a[i] = i;
+         }
+         start = clock();
+         shell(a, n);
+         stop = clock();
+         fprintf(t_measure, "%d) Time of shell (inc | decr): %lf\n",i , (stop - start )/ CLOCKS_PER_SEC);
+         
+         for (int i = 0; i < (n / 2); i++)
+         {
+            a[i] = i;
+         }
+         start = 0, stop = 0;
+         start = clock();
+         qs(a, 0, n - 1);
+         stop = clock();
+         fprintf(t_measure, "%d) Time of qs (inc | decr): %lf\n", i, (stop - start )/ CLOCKS_PER_SEC);
+         
+         for (int i = 0; i < (n / 2); i++)
+         {
+            a[i] = i;
+         }
+         start = 0, stop = 0;
+         
+         start = clock();
+         qsort(a, n, sizeof(int), funccmp);
+         stop = clock();
+         fprintf(t_measure, "%d) Time of qsort() (inc | decr): %lf\n", i, (stop - start )/ CLOCKS_PER_SEC);
+         break;
+      }
+      fclose(t_measure);
+      free(a);
    }
-   for (int i = (n / 2); i > 0; i--)
-   {
-      a[i] = i;
-   }
-   start = clock();
-   qsort(a, n, sizeof(int), funccmp);
-   stop = clock();
-
-   printf("\nTime of sort (qs): %lf sec", (stop - start) / CLOCKS_PER_SEC);
-   free(a);
+   puts("end.");
    getchar();
    return 0;
 }
