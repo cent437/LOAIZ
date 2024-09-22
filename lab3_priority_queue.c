c55a0616ec65d35c9db871a598b12a6e7e53a01f
Linus Torvalds <torvalds@linux-foundation.org>
2024-09-22 19:46:26 +0300

    .

---
+++ lab3_priority_queue.c
@@ -0,0 +1,119 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <locale.h>
+typedef struct node
+{
+   char inf[256]; // полезная информация
+   int prior;
+   struct node *next; // ссылка на следующий элемент
+}node;
+node *head = NULL, *last = NULL, *f = NULL; // указатели на первый и последний элементы списка
+int dlinna = 0;
+
+struct node* get_struct(void)
+{
+   node *p = NULL;
+   char s[256];
+   if ((p = (node*)malloc(sizeof(node))) == NULL) // выделяем память под новый элемент списка
+   {
+      puts("Error allocating memory");
+      exit(1);
+   }
+   puts("Enter object name:"); // вводим данные
+   scanf("%s", s);
+   if (*s == 0)
+   {
+      puts("The recording wasn't made"); // вводим данные
+      return NULL;
+   }
+   strcpy(p->inf, s);
+   p->prior = strlen(p->inf);
+   p->next = NULL;
+   return p; // возвращаем указатель на созданный элемент
+}
+/* Последовательное добавление в список элемента (в конец)*/
+
+void push(void)
+{
+   struct node *p = NULL;
+   p = get_struct();
+   if (head == NULL && p != NULL) // если списка нет, то устанавливаем голову списка
+   {
+      head = p;
+      last = p;
+   }
+   else if (head != NULL && p != NULL) // список уже есть, то вставляем в конец
+   {
+      if (p->prior > strlen(last->inf))
+      {
+         last->next = p;
+         last = p;
+      }
+      else
+      {
+         
+      }
+      
+   }
+   return;
+}
+void print(void)
+{
+   node *struc = head;
+   if (head == NULL)
+   {
+      puts("Queue is empty.");
+   }
+   printf("->\t");
+   while (struc)
+   {
+      printf("%s \t", struc->inf);
+      struc = struc->next;
+   }
+   return;
+}
+
+/*Удаление элемента в конце очереди*/
+void pop(void)
+{
+   struct node *struc = head; // указатель, проходящий по очереди установлен на начало списка
+   if (head == NULL) // если голова списка равна NULL, то список пуст
+      return;
+   
+   head = struc->next; // установливаем голову на следующий элемент
+   free(struc); // удаляем первый элемент
+   struc = head; // устанавливаем указатель для продолжения поиска
+}
+
+
+int main()
+{
+   setlocale(LC_ALL, "ru_RU.UTF-8");
+   int n;
+   puts("Enter queue size:");
+   scanf("%d", &n);
+   rewind(stdin);
+   for (int i = 0; i < n; i++)
+   {
+      push();
+   }
+   puts("Queue is full.");
+   print();
+   puts("\n1. Delete");
+   puts("0. Finish");
+   
+   while(n != 0)
+   {
+      scanf("%d", &n);
+      rewind(stdin);
+      if (n == 1)
+         pop();
+
+      print();   
+      puts("\n1. Delete");
+      puts("0. Finish");
+   }
+
+   return 0;
+}
