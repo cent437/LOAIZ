d5fccadd04197e3b1f2a27a03543664e1e461aa9
Эмир Ториев <torimir1977@yandex.ru>
2024-10-10 22:07:08 +0300

    fix matrix

--- graph.c
+++ graph.c
@@ -3,62 +3,60 @@
 #include <time.h>
 #include <stdint.h>
 #include <iso646.h>
-
+/* Печать матрицы смежности. Возвращает количество вершин графа.*/
+int32_t print_matrix(int32_t** matrix, int32_t matrix_size);
 int main()
 {
-	int32_t **a; // Матрица смежности
-	int32_t *b; // Массив степеней ребер
-	int32_t n = 0; // Размер матрицы смежности
-	int32_t dom; // Переменная для доминирующей вершины
-	int32_t size = 0;
-	puts("Введите размер матрицы смежности: ");
-	scanf("%d", &n);
-
-	for (int32_t i = 0; i < n; i++) // Выделение памяти под матрицу смежности и под массив степеней вершин
-		a = (int32_t**)calloc(n, sizeof(int32_t*));
-	for (int32_t i = 0; i < n; i++) 								
-		a[i] = (int32_t*)calloc(n, sizeof(int32_t));
-	b = (int32_t*)calloc(n, sizeof(int32_t));
-
-
+	int32_t **matrix;
+	int32_t *powers;
+	int32_t matrix_size = 0;
+	int32_t dom;
+	int32_t graph_size = 0;
 	srand(time(NULL));
-	puts("Матрица смежности для графа G:");
-	putchar('\t');
-	for (int32_t i = 0; i < n; i++)
-		printf("%d\t", i + 1);
-	puts("\n");
-	
-	for (int32_t i = 0; i < n; i++) {
-		printf("%d\t", i + 1);
-		for (int32_t j = 0; j < n; j++) {
-			a[i][j] = rand() % 2;
-			a[j][i] = a[i][j]; // Создаем связь между двумя вершинами
-			if (i == j) // если элемент матрицы в главной диагонали
-				a[i][j] = 0; 
-			if (a[i][j] == a[j][i]) // если есть ребро
-				size++;
-			b[i] += a[i][j]; // Подсчитываем степень вершин графа
+	puts("Введите размер матрицы смежности: ");
+	scanf("%d", &matrix_size);
 
-			printf("%d\t", a[i][j]);
+	/* Выделение памяти под матрицу смежности и под массив степеней вершин графа */
+	for (int32_t i = 0; i < matrix_size; i++) {
+		matrix= (int32_t**)calloc(matrix_size, sizeof(int32_t*));
+		for (int32_t j = 0; j < matrix_size; j++) {
+			matrix[j] = (int32_t*)calloc(matrix_size, sizeof(int32_t));
 		}
 		
-		putchar('\n');
+	}						
+	powers = (int32_t*)calloc(matrix_size, sizeof(int32_t));
+	for (int32_t i = 0; i < (matrix_size / 2); i++) {
+		for (int32_t j = 0; j < matrix_size; j++) {
+			matrix[i][j] = rand() % 2;  // Создаем связь между двумя вершинами
+			matrix[j][i] = matrix[i][j];
+			if (i == j) 
+				matrix[i][j] = 0; 
+			powers[i] += matrix[i][j]; // Подсчитываем степень вершин графа
+		}
 	}
 	putchar('\n');
-	
-		for (int32_t i = 0; i < n; i++)
-		{
-			if (b[i] > dom)
-				dom = b[i];
-			printf("deg(v[%d]) = %d", i + 1, b[i]);
-			if (b[i] == 0) 
-				printf("\tизолированная.");
-			else if (b[i] == 1)
-				printf("\tконцевая.");
-			//else if()
-				//puts("Доминирующая");
-			putchar('\n');
-		}
-		printf("Размер графа: %d", size);
+	graph_size = print_matrix(matrix, matrix_size) / 2;
+	printf("Размер графа: %d", graph_size);
 	return 0;
+}
+int32_t print_matrix(int32_t** matrix, int32_t matrix_size)
+{
+	int32_t size;
+	puts("Матрица смежности для графа G:");
+	putchar('\t');
+	for (int32_t i = 0; i < matrix_size; i++) 
+		printf("%d\t", i + 1);
+	putchar('\n');
+
+	for (int32_t i = 0; i < matrix_size; i++) {
+		printf("%d\t", i + 1);
+		for (int32_t j = 0; j < matrix_size; j++) {
+			
+			printf("%d\t", matrix[i][j]);
+			if (matrix[i][j] == 1 and matrix[j][i] == 1) // если есть ребро
+				size++;
+		}
+		putchar('\n');
+	}
+	return matrix_size;
 }
