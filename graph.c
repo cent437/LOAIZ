552960f2714c8b6e377d4064469d6aaf5d152c48
Эмир Ториев <torimir1977@yandex.ru>
2024-10-11 09:31:37 +0300

    adding powers

--- graph.c
+++ graph.c
@@ -1,62 +1,73 @@
+#include <iso646.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdint.h>
-#include <iso646.h>
-/* Печать матрицы смежности. Возвращает количество вершин графа.*/
-int32_t print_matrix(int32_t** matrix, int32_t matrix_size);
-int main()
-{
-	int32_t **matrix;
-	int32_t *powers;
-	int32_t matrix_size = 0;
-	int32_t dom;
-	int32_t graph_size = 0;
-	srand(time(NULL));
-	puts("Введите размер матрицы смежности: ");
-	scanf("%d", &matrix_size);
+/* Печать матрицы смежности. Возвращает количество вершин графа.
+ */
+int32_t print_matrix(int32_t **matrix, int32_t matrix_size);
+int main() {
+  int32_t **matrix, *powers, matrix_size = 0, dom, graph_size = 0;
+  srand(time(NULL));
+  puts("Введите размер матрицы смежности: ");
+  scanf("%d", &matrix_size);
+  /* Выделение памяти под матрицу смежности и под массив степеней вершин графа
+   */
+  for (int32_t i = 0; i < matrix_size; i++) {
+    matrix = (int32_t **)calloc(matrix_size, sizeof(int32_t *));
+    for (int32_t j = 0; j < matrix_size; j++) {
+      matrix[j] = (int32_t *)calloc(matrix_size, sizeof(int32_t));
+    }
+  }
+  powers = (int32_t *)calloc(matrix_size, sizeof(int32_t));
+  for (int32_t i = 0; i < (matrix_size / 2); i++) {
+    for (int32_t j = 0; j < matrix_size; j++) {
+      matrix[i][j] = rand() % 2; // Создаем связь между двумя вершинами
+      matrix[j][i] = matrix[i][j];
+      if (i == j)
+        matrix[i][j] = 0;
+    }
+  }
+  for (int32_t i = 0; i < matrix_size; i++) {
+    for (int32_t j = 0; j < matrix_size; j++)
+      powers[i] += matrix[i][j]; // Подсчитываем степени вершин графа
+  }
 
-	/* Выделение памяти под матрицу смежности и под массив степеней вершин графа */
-	for (int32_t i = 0; i < matrix_size; i++) {
-		matrix= (int32_t**)calloc(matrix_size, sizeof(int32_t*));
-		for (int32_t j = 0; j < matrix_size; j++) {
-			matrix[j] = (int32_t*)calloc(matrix_size, sizeof(int32_t));
-		}
-		
-	}						
-	powers = (int32_t*)calloc(matrix_size, sizeof(int32_t));
-	for (int32_t i = 0; i < (matrix_size / 2); i++) {
-		for (int32_t j = 0; j < matrix_size; j++) {
-			matrix[i][j] = rand() % 2;  // Создаем связь между двумя вершинами
-			matrix[j][i] = matrix[i][j];
-			if (i == j) 
-				matrix[i][j] = 0; 
-			powers[i] += matrix[i][j]; // Подсчитываем степень вершин графа
-		}
-	}
-	putchar('\n');
-	graph_size = print_matrix(matrix, matrix_size) / 2;
-	printf("Размер графа: %d", graph_size);
-	return 0;
+  putchar('\n');
+  graph_size = print_matrix(matrix, matrix_size) / 2;
+  puts("Степени вершин графа:");
+  for (int32_t i = 0; i < matrix_size; i++) {
+    printf("deg[%d] = %d", i + 1, powers[i]);
+    if (powers[i] == 1)
+      printf(" концевая.");
+    if (powers[i] == 0)
+      printf(" изолированная.");
+    putchar('\n');
+    if (dom > powers[i])
+      dom = powers[i];
+  }
+
+  printf("Размер графа: %d", graph_size);
+  return 0;
 }
-int32_t print_matrix(int32_t** matrix, int32_t matrix_size)
-{
-	int32_t size;
-	puts("Матрица смежности для графа G:");
-	putchar('\t');
-	for (int32_t i = 0; i < matrix_size; i++) 
-		printf("%d\t", i + 1);
-	putchar('\n');
+int32_t print_matrix(int32_t **matrix, int32_t matrix_size) {
+  int32_t size;
+  puts("Матрица смежности для графа G:");
+  putchar(' ');
+  putchar(' ');
+  for (int32_t i = 0; i < matrix_size; i++)
+    printf("%d ", i + 1);
+  putchar('\n');
 
-	for (int32_t i = 0; i < matrix_size; i++) {
-		printf("%d\t", i + 1);
-		for (int32_t j = 0; j < matrix_size; j++) {
-			
-			printf("%d\t", matrix[i][j]);
-			if (matrix[i][j] == 1 and matrix[j][i] == 1) // если есть ребро
-				size++;
-		}
-		putchar('\n');
-	}
-	return matrix_size;
+  for (int32_t i = 0; i < matrix_size; i++) {
+    printf("%d ", i + 1);
+    for (int32_t j = 0; j < matrix_size; j++) {
+
+      printf("%d ", matrix[i][j]);
+      if (matrix[i][j] == 1 and matrix[j][i] == 1) // если есть ребро
+        size++;
+    }
+    putchar('\n');
+  }
+  return matrix_size;
 }
