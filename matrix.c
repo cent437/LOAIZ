a50ad76244abb5d4c456c87ca7cbc29a6aa31265
Эмир Ториев <torimir1977@yandex.ru>
2024-10-15 14:20:56 +0300

    optimization

--- matrix.c
+++ matrix.c
@@ -6,23 +6,16 @@
 int32_t **generate_incident_matrix(int32_t matrix_size) {
 
   int32_t **incident_matrix;
-
-  for (int32_t i = 0; i < matrix_size; i++) {
-    incident_matrix = (int32_t **)calloc(matrix_size, sizeof(int32_t *));
-
-    for (int32_t j = 0; j < matrix_size; j++)
-      incident_matrix[j] = (int32_t *)calloc(matrix_size, sizeof(int32_t));
-  }
+  incident_matrix = (int32_t **)calloc(matrix_size, sizeof(int32_t *));
+  for (int32_t i = 0; i < matrix_size; i++)
+    incident_matrix[i] = (int32_t *)calloc(matrix_size, sizeof(int32_t));
   return incident_matrix;
 }
 int32_t **generate_adjacency_matrix(int32_t matrix_size) {
-  int32_t **adjacency_matrix;
-  for (int32_t i = 0; i < matrix_size; i++) {
-    adjacency_matrix = (int32_t **)calloc(matrix_size, sizeof(int32_t *));
-
-    for (int32_t j = 0; j < matrix_size; j++)
-      adjacency_matrix[j] = (int32_t *)calloc(matrix_size, sizeof(int32_t));
-  }
+  int32_t **adjacency_matrix =
+      (int32_t **)calloc(matrix_size, sizeof(int32_t *));
+  for (int32_t i = 0; i < matrix_size; i++)
+    adjacency_matrix[i] = (int32_t *)calloc(matrix_size, sizeof(int32_t));
   for (int32_t i = 0; i < (matrix_size / 2); i++) {
 
     for (int32_t j = 0; j < matrix_size; j++) {
@@ -38,7 +31,6 @@ int32_t print_adjacency_matrix(int32_t **adjacency_matrix,
                                int32_t matrix_size) {
 
   int32_t size;
-
   puts("Матрица смежности для графа G:");
   putchar(' ');
   putchar(' ');
@@ -53,7 +45,8 @@ int32_t print_adjacency_matrix(int32_t **adjacency_matrix,
     for (int32_t j = 0; j < matrix_size; j++) {
 
       printf("%d ", adjacency_matrix[i][j]);
-      if (adjacency_matrix[i][j] == 1 && adjacency_matrix[j][i] == 1) // если есть ребро
+      if (adjacency_matrix[i][j] == 1 &&
+          adjacency_matrix[j][i] == 1) // если есть ребро
         size++;
     }
     putchar('\n');
@@ -72,13 +65,11 @@ int32_t print_incident_matrix(int32_t **incident_matrix, int32_t matrix_size) {
 
   for (int32_t i = 0; i < matrix_size; i++) {
     printf("%d ", i + 1);
-
     for (int32_t j = 0; j < matrix_size; j++) {
       printf("%d ", incident_matrix[i][j]);
       if (incident_matrix[i][j] == 1)
         size++;
     }
-
     putchar('\n');
   }
   return size;
