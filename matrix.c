ab1354f279c30c3e7427ed7efcd84fb655e17bcc
Эмир Ториев <torimir1977@yandex.ru>
2024-10-25 12:21:08 +0300

    adding vertex idem

--- matrix.c
+++ matrix.c
@@ -1,6 +1,4 @@
 #include "matrix.h"
-#include <stdatomic.h>
-#include <stdint.h>
 
 int32_t **generate_incident_matrix(int32_t rows, int32_t cols) {
   int32_t **incident_matrix;
@@ -19,14 +17,11 @@ int32_t **generate_adjacency_matrix(int32_t matrix_size) {
   for (int32_t i = 0; i < matrix_size; i++)
     adjacency_matrix[i] = (int32_t *)calloc(matrix_size, sizeof(int32_t));
   for (int32_t i = 0; i < (matrix_size / 2); i++) {
-
     for (int32_t j = 0; j < matrix_size; j++) {
       adjacency_matrix[i][j] = rand() % 2;
-      adjacency_matrix[i][i] = 0;
       adjacency_matrix[j][i] = adjacency_matrix[i][j];
-      // if (i == j)
-      // adjacency_matrix[i][j] = 0;
     }
+    adjacency_matrix[i][i] = 0;
   }
   return adjacency_matrix;
 }
@@ -62,25 +57,35 @@ int32_t print_adjacency_matrix(int32_t **adjacency_matrix,
   return size;
 }
 int32_t **otozhd(int32_t **G, int32_t size, int32_t V) {
-  if (V > size) {
-    printf("Вершины %d не существует", V);
-    return G;
+  int32_t **G_new = NULL;
+  if ((V + 1) > size) {
+    printf("Вершины %d не существует.\n", V);
+    exit(1);
   }
-  int32_t **G_new = generate_adjacency_matrix(size - 1);
+  G_new = generate_adjacency_matrix(size - 1);
   for (int32_t i = 0; i < size; i++) {
     for (int32_t j = 0; j < size; j++) {
-      if (i < V and j < V) {
+      /* Если удаляемая вершина - левый верхний
+      или правый нижний угол матрицы смежности */
+      if (i == V and j == V) {
+        for (i = 0; i < (size - 1); i++) {
+          for (j = 0; j < (size - 1); j++)
+            G_new[i][j] = G[i + 1][j + 1];
+        }
+        return G_new;
+      }
+
+      if (i < V and j < V)
         G_new[i][j] = G[i][j];
-      }
-      if (i > V and j > V) {
+
+      if (i > V and j > V)
         G_new[i - 1][j] = G[i][j];
-      }
-      if (i < V and j > V) {
+
+      if (i < V and j > V)
         G_new[i][j - 1] = G[i][j];
-      }
-      if (i > V and j < V) {
+
+      if (i > V and j < V)
         G_new[i - 1][j] = G[i][j];
-      }
     }
   }
   return G_new;
