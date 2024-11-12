e6e51c2171e19481bb8dce3dd500362c7af37268
Эмир Ториев <torimir1977@yandex.ru>
2024-11-12 16:28:47 +0300

    lab_6

--- tasks/main.c
+++ tasks/main.c
@@ -1,57 +1,147 @@
 #include "../graph.h"
-
 #include <stdint.h>
-#include <time.h>
-
-void dfs_list(list **l, int32_t start, int32_t *visited, int32_t size) {
-  list *head = l[start];
-  visited[start] = 1;
-  printf("%d ", start + 1);
-  while (head != NULL) {
-    if (visited[head->index - 1] == 0) {
-      dfs_list(l, head->index - 1, visited, size);
-    }
-    head = head->next;
-  }
-}
-
-void dfs_matrix(int32_t **G, int32_t start, int32_t *visited, int32_t size) {
-  visited[start] = 1;
-  printf("%d ", start + 1);
-  for (int i = 0; i < size; i++) {
-    if (G[start][i] == 1 and visited[i] == 0) {
-      dfs_matrix(G, i, visited, size);
-    }
-  }
-  return;
-}
-
-int32_t main() {
-
-  int32_t size, vertex, **G = NULL, *visited = NULL;
-  puts("Введите размер матрицы смежности: ");
-  scanf("%d", &size);
-
-  G = generate_adjacency_matrix(size);
-  visited = calloc(size, sizeof(int32_t));
-  print_adjacency_matrix(G, size);
-  list **l = create_adjacency_list(G, size);
-
-  /* Печать списков смежности */
-  print_adjacency_list(l, size);
-
-  puts("Введите номер стартовой вершины: ");
-  scanf("%d", &vertex);
+#include <stdio.h>
+int main() {
+  int32_t **G1 = NULL, **G2 = NULL, **G3 = NULL, size_G1 = 0, size_G2 = 0,
+          size_G3 = 0, v1 = 0, v2 = 0, ui_switch = -1, graph_num = 0,
+          sizemax = 0, sizemin = 0;
+  list **adjacency_list = NULL;
+  puts("Создание графов G1 и G2.");
+  puts("Введите размер матриц:");
+  scanf("%d %d", &size_G1, &size_G2);
+  sizemax = (size_G1 > size_G2) ? size_G1 : size_G2;
+  sizemin = (size_G1 < size_G2) ? size_G1 : size_G2;
+  G1 = generate_adjacency_matrix(size_G1);
+  print_adjacency_matrix(G1, size_G1);
   putchar('\n');
-  for (int32_t i = 0; i < size; i++)
-    visited[i] = 0;
-  puts("Пройденный маршрут:");
-  // dfs_matrix(G, vertex - 1, visited, size);
-  dfs_list(l, vertex - 1, visited, size);
-  // for (int32_t i = 0; i < size; i++)
-  // free(G[i]);
+  G2 = generate_adjacency_matrix(size_G2);
+  print_adjacency_matrix(G2, size_G2);
+  while (ui_switch != 0) {
+    switch (ui_switch) {
+    case 1: /* Отождествление вершин */
+      puts("Выберите граф для операции.");
+      puts("1 - G1.");
+      puts("2 - G2.");
+      scanf("%d", &graph_num);
+      switch (graph_num) {
+      case 1:
+        puts("Введите индексы вершин через пробел");
+        scanf("%d %d", &v1, &v2);
+        G1 = unionV(G1, size_G1, v1, v2);
+        print_adjacency_matrix(G1, size_G1 - 1);
+        break;
+      case 2:
+        puts("Введите индексы вершин через пробел");
+        scanf("%d %d", &v1, &v2);
+        G1 = unionV(G1, size_G2, v1, v2);
+        print_adjacency_matrix(G1, size_G2 - 1);
+        break;
+      }
+      break;
+    case 2: /* Стягивание ребра */
+      puts("Выберите граф для операции.");
+      puts("1 - G1.");
+      puts("2 - G2.");
+      scanf("%d", &graph_num);
+      switch (graph_num) {
+      case 1:
+        if (G1 == NULL) {
+          puts("Графа G1 не существует");
+          break;
+        }
+        puts("Введите индексы вершин через пробел");
+        scanf("%d %d", &v1, &v2);
+        G3 = contrV(G1, size_G1, v1, v2);
+        print_adjacency_matrix(G3, size_G1 - 1);
+        break;
+      case 2:
+        if (G2 == NULL) {
+          puts("Графа G2 не существует");
+          break;
+        }
+        puts("Введите индексы вершин через пробел");
+        scanf("%d %d", &v1, &v2);
+        G3 = contrV(G2, size_G2, v1, v2);
+        print_adjacency_matrix(G3, size_G2 - 1);
+        break;
+      }
+      break;
+    case 3: /* Расщепление вершин */
+      puts("Выберите граф для операции.");
+      puts("1 - G1.");
+      puts("2 - G2.");
+      scanf("%d", &graph_num);
+      switch (graph_num) {
+      case 1:
+        puts("Введите индексы вершин через пробел");
+        scanf("%d", &v1);
+        G3 = splitV(G1, size_G1, v1);
+        print_adjacency_matrix(G3, size_G1 + 1);
+        break;
+      case 2:
+        puts("Введите индексы вершин через пробел");
+        scanf("%d", &v1);
+        G3 = splitV(G2, size_G2, v1);
+        print_adjacency_matrix(G3, size_G2 + 1);
+        break;
+      }
+      break;
+    case 4: /* Объединение графов */
+      puts("G1 ∪ G2:");
+      G3 = unionG(G1, G2, size_G1, size_G2);
+      print_adjacency_matrix(G3, sizemax);
+      break;
+    case 5: /* Пересечение графов */
+      puts("G1 ∩ G2:");
+      G3 = intersectionG(G1, G2, size_G1, size_G2);
+      print_adjacency_matrix(G3, sizemin);
+      break;
+    case 6: /* Кольцевая сумма */
+      puts("G1⊕ G2:");
+      G3 = xor_G(G1, G2, size_G1, size_G2, &size_G3);
+      print_adjacency_matrix(G3, size_G3);
+      break;
+    case 7: /* Список смeжности */
+      puts("Выберите граф для операции.");
+      puts("1 - G1.");
+      puts("2 - G2.");
+      int32_t t;
+      scanf("%d", &graph_num);
+      switch (graph_num) {
+      case 1:
+        adjacency_list = create_adjacency_list(G1, size_G1);
+        break;
+      case 2:
+        adjacency_list = create_adjacency_list(G2, size_G2);
+        break;
+      }
+      break;
+    }
 
-  // free(G);
-  //  free(visited);
+    putchar('\n');
+    putchar('\n');
+    puts("1. Выполнить отождествление вершин.");
+    puts("2. Выполнить стягивание ребра.");
+    puts("3. Выполнить расщепление вершин.");
+    puts("4. Выполнить объединение двух графов.");
+    puts("5. Выполнить пересечение двух графов.");
+    puts("6. Выполнить кольцевую суммц двух графов.");
+    puts("7. Создать список смежности.");
+    puts("0. Выход.");
+    printf("-> ");
+    scanf("%d", &ui_switch);
+  }
+
+  if (!G1) {
+    for (int32_t i = 0; i < size_G1; i++)
+      free(G1[i]);
+    free(G1);
+  }
+  if (!G2) {
+    for (int32_t i = 0; i < size_G2; i++)
+      free(G2[i]);
+    free(G2);
+  }
+
   return 0;
 }
