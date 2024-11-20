ce78fee5a687508f6db8e3a83830ba4d77fe79e2
Эмир Ториев <torimir1977@yandex.ru>
2024-11-20 15:35:41 +0300

    add sort

--- tasks/main.cpp
+++ tasks/main.cpp
@@ -1,5 +1,35 @@
 #include "../graph.h"
 #include <cstdio>
+#include <cstdlib>
+void sort(int *a, int size) {
+  int *r = (int *)calloc(size, sizeof(int));
+  for (int i = 0; i < size; i++) {
+    r[i] = i;
+  }
+  for (int i = 1; i < size; i++) {
+    int k = i;
+    while (k > 0 and a[k - 1] > a[k]) {
+
+      a[k - 1] ^= a[k];
+      r[k - 1] ^= r[k];
+
+      a[k] ^= a[k - 1];
+      r[k] ^= r[k - 1];
+
+      a[k - 1] ^= a[k];
+      r[k - 1] ^= r[k];
+
+      k--;
+    }
+  }
+  for (int i = 0; i < size; i++) {
+    printf("%d\t", r[i] + 1);
+  }
+  putchar(10);
+  for (int i = 0; i < size; i++) {
+    printf("%d\t", a[i]);
+  }
+}
 
 int main() {
   int **G = NULL, *visited = NULL, *dist = NULL, size = 0, vertex = 0;
@@ -63,30 +93,24 @@ int main() {
   for (int i = 0; i < size; i++)
     visited[i] = 0;
 
+  puts("В результате оценки времени обхода в ширину контейнер queue работает "
+       "быстрее на больших графах, а класс очереди на малых графах.");
+
   puts("===================== Лабораторная работа №9 =====================");
   puts("---Поиск расстояний по матрице смежности:");
   bfsd_matrix(G, vertex - 1, size, dist);
   printf("\nВектор расстояний от вершины %d до других смежных вершин\n",
          vertex);
-
+  sort(dist, size);
   for (int i = 0; i < size; i++)
-    printf("%d\t", i + 1);
-  putchar(10);
-  for (int i = 0; i < size; i++) {
-    printf("%d\t", dist[i]);
     dist[i] = -1;
-  }
+
   puts("\n---Поиск расстояний по списку смежности:");
   bfsd_list(l, vertex - 1, size, dist);
-
   printf("\nВектор расстояний от вершины %d до других смежных вершин\n",
          vertex);
+  sort(dist, size);
   for (int i = 0; i < size; i++)
-    printf("%d\t", i + 1);
-  putchar(10);
-  for (int i = 0; i < size; i++) {
-    printf("%d\t", dist[i]);
     dist[i] = -1;
-  }
   return 0;
 }
