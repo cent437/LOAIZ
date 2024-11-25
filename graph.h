5dbe21b596363431a4d2cec19add8cb77389766b
Эмир Ториев <torimir1977@yandex.ru>
2024-11-25 20:02:32 +0300

    refactoring

--- graph.h
+++ graph.h
@@ -3,7 +3,6 @@
  */
 
 #include <cstddef>
-#include <cstdint>
 #include <iso646.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -17,24 +16,22 @@
  */
 typedef struct adjacency_list {
   int32_t index;
-  int32_t weight;
   struct adjacency_list *next;
   struct adjacency_list *prev;
 
 } list;
-
 class c_stack {
 public:
   typedef struct stack {
     struct stack *next;
+    struct stack *prev;
     int data;
   } stack;
   stack *head = NULL, *tail = NULL;
-  stack *create_stack_node(int data);
+  stack *create_node(int data);
   void push(int data);
   void pop();
 };
-
 class c_queue {
 public:
   typedef struct node {
@@ -43,7 +40,7 @@ public:
     int data;
   } node;
   node *head = NULL, *tail = NULL;
-  node *create_node(int32_t data);
+  node *create_node(int data);
   void push(int data);
   void pop();
 };
@@ -51,69 +48,21 @@ public:
 /* Генерация матрицы смежности размером matrix_size. */
 extern int32_t **generate_adjacency_matrix(int32_t matrix_size);
 
-/* Генерация матрицы смежности для взвешенного графа */
-extern int32_t **generate_adjacency_matrix_weight(int32_t matrix_size);
-extern int32_t **generate_adjacency_matrix_orient(int32_t matrix_size);
-extern int32_t **generate_adjacency_matrix_orient_weight(int32_t matrix_size);
-
-/* Выделение памяти под матрицу инцидентности размером matrix_size. */
-extern int32_t **generate_incident_matrix(int32_t rows, int32_t cols);
-
 /* Печать матрицы смежности. Возвращает размер графа. */
 extern void print_adjacency_matrix(int32_t **adjacency_matrix,
                                    int32_t matrix_size);
 
-/* Печать матрицы инцидентности. */
-extern void print_incident_matrix(int32_t **incident_matrix, int32_t rows,
-                                  int32_t cols);
-
-/* Удаление вершины V в графе G. */
-extern int32_t **del_vertex(int32_t **G, int32_t size, int32_t V);
-
-/* Отождествление вершин v1 и v2 в графе G. */
-extern int32_t **unionV(int32_t **G, int32_t size, int32_t v1, int32_t v2);
-
-/* Стягивание ребра, соединяющего вершины v1 и v2 в графе G. */
-extern int32_t **contrV(int32_t **G, int32_t size, int32_t v1, int32_t v2);
-
-/* Расщепление вершины v в графе G. */
-extern int32_t **splitV(int32_t **G, int32_t size, int32_t v);
-
-extern int32_t **contrE(int32_t **G, int32_t size, int32_t v1, int32_t v2);
-
-/* Объединение графов G1 и G2. */
-extern int32_t **unionG(int32_t **G1, int32_t **G2, int32_t size_G1,
-                        int32_t size_G2);
-
-/* Пересечение графов G1 и G2. */
-extern int32_t **intersectionG(int32_t **G1, int32_t **G2, int32_t size_G1,
-                               int32_t size_G2);
-/* Кольцевая сумма графов G1 и G2. */
-extern int32_t **xor_G(int32_t **G1, int32_t **G2, int32_t size_G1,
-                       int32_t size_G2, int32_t *size_Gtemp);
-
 /* Выделение памяти под новый узел. */
-extern list *create_node(int32_t data, int32_t weight);
+extern list *create_node(int32_t data);
 
 /* Запись смежной вершины в список смежности. */
-extern void push(list **lists_pointer, int32_t data, int32_t weight,
-                 int32_t head_index);
+extern void push(list **lists_pointer, int32_t data, int32_t head_index);
 
 /* Создание списка смежности на основе матрицы смежности. */
 extern list **create_adjacency_list(int32_t **G, int32_t size);
 
 /* Печать списка смежности. */
 extern void print_adjacency_list(list **lists_pointer, int32_t size);
-
-/* Обход в глубину по матрице смежности */
-extern void dfs_matrix(int32_t **G, int32_t start, int32_t *visited,
-                       int32_t size);
-
-/* Обход в глубину по списку смежности */
-extern void dfs_list(list **l, int32_t start, int32_t *visited, int32_t size);
-
-void dfs_no_recursive(int32_t **G, int32_t start, int32_t *visited,
-                      int32_t size);
 
 /* Обход в ширину по матрице смежности. */
 extern void bfs_matrix(int32_t **G, int32_t size, int32_t *visited, int32_t v);
@@ -123,17 +72,3 @@ extern void bfs_list(list **l, int32_t size, int32_t *visited, int32_t v);
 
 /* Обход в ширину с использованием собственной очереди. */
 extern void c_bfs(int32_t **G, int32_t size, int32_t *visited, int32_t v);
-
-/* Поиск расстояний методом поиска в ширину по матрице смежности */
-extern void bfsd_matrix(int32_t **G, int32_t v, int32_t size, int32_t *dist);
-
-/* Поиск расстояний методом поиска в ширину в списке смежности с использованием
- * контейнера queue */
-extern void bfsd_list(list **l, int32_t v, int32_t size, int32_t *dist);
-
-/* Поиск расстояний методом поиска в ширину в списке смежности с использованием
- * собственного класса очереди */
-extern void c_bfsd_list(list **l, int32_t v, int32_t size, int32_t *dist);
-
-/* Вывод диаметра и радиуса взвешенного графа. */
-extern void diameter_radius(int32_t **G_weight, int32_t size);
--- tasks/main.cpp
+++ tasks/main.cpp
@@ -1,38 +1,8 @@
 #include "../graph.h"
 #include <cstdio>
-#include <cstdlib>
-void sort(int *a, int size) {
-  int *r = (int *)calloc(size, sizeof(int));
-  for (int i = 0; i < size; i++) {
-    r[i] = i;
-  }
-  for (int i = 1; i < size; i++) {
-    int k = i;
-    while (k > 0 and a[k - 1] > a[k]) {
-
-      a[k - 1] ^= a[k];
-      r[k - 1] ^= r[k];
-
-      a[k] ^= a[k - 1];
-      r[k] ^= r[k - 1];
-
-      a[k - 1] ^= a[k];
-      r[k - 1] ^= r[k];
-
-      k--;
-    }
-  }
-  for (int i = 0; i < size; i++) {
-    printf("%d\t", r[i] + 1);
-  }
-  putchar(10);
-  for (int i = 0; i < size; i++) {
-    printf("%d\t", a[i]);
-  }
-}
 
 int main() {
-  int **G = NULL, *visited = NULL, *dist = NULL, size = 0, vertex = 0;
+  int **G = NULL, *visited = NULL, size = 0, vertex = 0;
   list **l = NULL;
   puts("Введите размер матрицы смежности:");
   scanf("%d", &size);
@@ -46,35 +16,27 @@ int main() {
   print_adjacency_list(l, size);
 
   visited = (int *)calloc(size, sizeof(int));
-  dist = (int *)calloc(size, sizeof(int));
-  for (int i = 0; i < size; i++) {
-    dist[i] = -1;
-    visited[i] = 0;
-  }
-
   puts("Введите стартовую вершину:");
   scanf("%d", &vertex);
+
   puts(
       "Обход в ширину по матрице смежности с использованием контейнера queue:");
   bfs_matrix(G, size, visited, vertex - 1);
-  putchar(10);
   for (int i = 0; i < size; i++)
     visited[i] = 0;
-
-  puts("Обход в ширину по списку смежности с использованием контейнера queue:");
-  bfs_list(l, size, visited, vertex - 1);
   putchar(10);
-  for (int i = 0; i < size; i++)
-    visited[i] = 0;
 
-  puts("Обход в ширину с использованием собственного класса очереди:");
+  puts("Обход в ширину по матрице смежности с использованием класса c_queue:");
   c_bfs(G, size, visited, vertex - 1);
-  putchar(10);
   for (int i = 0; i < size; i++)
     visited[i] = 0;
+  putchar(10);
 
-  puts("В результате оценки времени обхода в ширину контейнер queue работает "
-       "быстрее на больших графах, а класс очереди на малых графах.");
+  puts("Обход в ширину по списку смежности:");
+  bfs_list(l, size, visited, vertex - 1);
+  for (int i = 0; i < size; i++)
+    visited[i] = 0;
+  putchar(10);
 
   return 0;
 }
